3faadc7ab1be0323919a8581dd3b5d1315ccedb0
amalnasr <amalalghanim96@gmail.com>
2021-09-06 23:48:08 +0300

    Update elif condition

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 def search(number):
-
     all_numbers = {'1111111111': 'Amal', '2222222222': 'Mohammed', '3333333333': 'Khadijah',
                    '4444444444': 'Abdullah', '5555555555': 'Rawan', '6666666666': 'Faisal',
                    '7777777777': 'Layla'}
 
     if number in all_numbers.keys():
-        print('This number for: '+all_numbers[number])
+        print('This number for: ' + all_numbers[number])
     elif (number.isdecimal() == False) or (len(number) != 10):
         print('This is invalid number')
     else:
@@ -15,5 +14,3 @@ def search(number):
 print('Welcome to Phone Directory!!!')
 phone_number = input('Enter the number you want:')
 search(phone_number)
-
-
